77c284e749dd2314446ce8453a2a2f17476c827f
he <trumanhe0@gmail.com>
2022-12-30 12:06:04 +0800

    refactor user context from useState to reducer

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,17 +1,39 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangeListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
 
 export const UserContext = createContext({
-    user: null,
-    setUser: ()=>null,
+  user: null,
+  setUser: ()=>null,
 });
 
+const User_Action_Types = {
+    SET_USER: "set_user",
+};
+
+const InitUserState = {
+    user: null
+};
+
+export const userReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case User_Action_Types.SET_USER: 
+      return {...state, user: payload};
+    default:
+      throw new Error("Unhandled action type: ", type);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-    const [ user, setUser ] = useState(null);
+    const [{ user }, dispatch ] = useReducer(userReducer, InitUserState);
+    const setUser = (user) => {
+      dispatch({type: User_Action_Types.SET_USER, payload: user});
+    }
+    
     const value = {user, setUser};
     useEffect(() => {
         const unsubscribe = onAuthStateChangeListener((user)=>{
-        // console.log("listener:", user);
+        console.log("listener:", user);
         setUser(user);
         if (user) {
             createUserDocumentFromAuth(user);
