695457204c24432abd760910d085d51ac7e3d0b3
he <trumanhe0@gmail.com>
2023-01-12 15:57:40 +0800

    migrate cart reducer to typescript, use memorize to improve performance of CartItem component

--- src/components/cart-item/cart-item.component.tsx
+++ src/components/cart-item/cart-item.component.tsx
@@ -1,7 +1,15 @@
 
-import { CartItemContainer, ItemDetails, Text } from './cart-item.styles';
 
-const CartItem = ({item})=> {
+import { FC, memo } from 'react';
+import { CartItemContainer, ItemDetails, Text } from './cart-item.styles';
+import { CartItem as CartItemT } from '../../store/cart/cart.slice';
+
+type CartItemProps = {
+    item: CartItemT,
+}
+
+// memo: unless props changed, would not rerender the CartItem component.
+const CartItem: FC<CartItemProps> = memo(({item})=> {
     const {name, price, imageUrl, quantity} = item;
     return (
     <CartItemContainer>
@@ -12,6 +20,6 @@ const CartItem = ({item})=> {
         </ItemDetails>
     </CartItemContainer>
     )
-};
+});
 
 export default CartItem;
